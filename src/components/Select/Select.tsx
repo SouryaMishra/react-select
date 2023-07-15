432deb190c120a1bfa39c1d863a16224169261a5
Sourya Mishra <sourya.mishra111@gmail.com>
2023-07-15 12:29:53 +0530

    Add custom label to component

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -70,56 +70,67 @@ export const Select: React.FC<ISelectProps> = ({
   };
 
   return (
-    <div className="select" ref={divRef}>
-      <button
-        aria-haspopup="listbox"
-        aria-expanded={isListShown ? "true" : "false"}
-        className={`select__trigger${isListShown ? " open" : ""}`}
-        onClick={() => {
-          setIsListShown((shown) => !shown);
-        }}
-        ref={buttonRef}
+    <div className="select">
+      <span
+        id="select-label"
+        style={{ display: "inline-block", marginBottom: 8 }}
       >
-        {selectedOption?.text || "Select Anime"}
-      </button>
-      {isListShown ? (
-        <ul
-          role="listbox"
-          tabIndex={-1}
-          className="select__list"
-          ref={listRef}
-          onKeyDown={handleKeyDown}
+        Select your favourite Anime
+      </span>
+      <div ref={divRef}>
+        <button
+          aria-haspopup="listbox"
+          aria-expanded={isListShown ? "true" : "false"}
+          aria-labelledby="select-label"
+          className={`select__trigger${isListShown ? " open" : ""}`}
+          onClick={() => {
+            setIsListShown((shown) => !shown);
+          }}
+          ref={buttonRef}
         >
-          {options.map(({ id, text, value, render }, index) => (
-            <li
-              role="option"
-              aria-selected={value === selectedOption?.value ? "true" : "false"}
-              key={id}
-              className={`select__list-option${
-                value === selectedOption?.value ? " selected" : ""
-              }`}
-              id={`option-${id}`}
-              data-id={id}
-              data-text={text}
-              data-value={value}
-              onClick={() => {
-                onSelect({ id, text, value });
-                setIsListShown(false);
-              }}
-              ref={(el) => {
-                if (el) listItemRefs.current[index] = el;
-              }}
-            >
-              {typeof render === "function"
-                ? render(
-                    { id, text, value },
-                    { emoji: index === 2 ? "😎" : "😂" }
-                  )
-                : text}
-            </li>
-          ))}
-        </ul>
-      ) : null}
+          {selectedOption?.text || "Select Anime"}
+        </button>
+        {isListShown ? (
+          <ul
+            role="listbox"
+            tabIndex={-1}
+            className="select__list"
+            ref={listRef}
+            onKeyDown={handleKeyDown}
+          >
+            {options.map(({ id, text, value, render }, index) => (
+              <li
+                role="option"
+                aria-selected={
+                  value === selectedOption?.value ? "true" : "false"
+                }
+                key={id}
+                className={`select__list-option${
+                  value === selectedOption?.value ? " selected" : ""
+                }`}
+                id={`option-${id}`}
+                data-id={id}
+                data-text={text}
+                data-value={value}
+                onClick={() => {
+                  onSelect({ id, text, value });
+                  setIsListShown(false);
+                }}
+                ref={(el) => {
+                  if (el) listItemRefs.current[index] = el;
+                }}
+              >
+                {typeof render === "function"
+                  ? render(
+                      { id, text, value },
+                      { emoji: index === 2 ? "😎" : "😂" }
+                    )
+                  : text}
+              </li>
+            ))}
+          </ul>
+        ) : null}
+      </div>
     </div>
   );
 };
